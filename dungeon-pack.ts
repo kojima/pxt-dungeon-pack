77658ef66cd4b0dea67038f31a3c533f08537f45
kojima <hide.k0712@gmail.com>
2023-07-05 20:14:53 -0700

    bug fix

--- dungeon-pack.ts
+++ dungeon-pack.ts
@@ -1,12 +1,13 @@
 /**
- * ダンジョンスターターパック
+ * ダンジョンパック
  */
-//% weight=150 color=#e67e22 icon="\uf3ed" block="ダンジョンスターターパック"
+//% weight=150 color=#e67e22 icon="\uf3ed" block="ダンジョンパック"
 namespace dungeon_pack {
+
     /**
      * タイル上にスプライトを生成する
      */
-    //% block="スプライト%sprite=screen_image_picker (%kind=spriteKind タイプ)をタイル%tile 上に生成する || (速度 vx:%vx , vy:%vy)"
+    //% block="スプライト%sprite=screen_image_picker (%kind=spritekind タイプ)をタイル%tile 上に生成する || (速度 vx:%vx , vy:%vy)"
     //% tile.shadow=tileset_tile_picker
     //% tile.decompileIndirectFixedInstances=true
     //% expandableArgumentMode="toggle"
@@ -14,7 +15,7 @@ namespace dungeon_pack {
     //% vx.defl=0
     //% vy.defl=0
     //% weight=97
-    export function spawnSpritesOnTile(sprite: Image, kind: number, tile: Image, vx: number = 0, vy: number = 0) {
+    export function spawnSpritesOnTiles(sprite: Image, kind: number, tile: Image, vx: number = 0, vy: number = 0) {
         tiles.getTilesByType(tile).forEach(tLoc => {
             const s = sprites.create(sprite, kind)
             tiles.placeOnTile(s, tLoc)
